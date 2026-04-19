7026c7fba16e0f01eee9b61252f2cdd3411c637d
collgriffz-sudo <collgriffz@gmail.com>
2026-04-19 17:40:26 +0300

    Update cart-core.js

--- cart-core.js
+++ cart-core.js
@@ -118,7 +118,8 @@
                             удалить
                         </button>
                     </div>
-                    `;
+                </div>
+            `;
         });
         
         itemsHtml += `
